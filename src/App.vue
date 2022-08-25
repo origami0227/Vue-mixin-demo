3d85e9002e09c57a1ab4dbfdac639c8b082b7e2d
origami <1071242996@qq.com>
2022-08-25 11:24:04 +0800

    mixin-demo

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+  </div>
+</template>
+
+<script>
+import log from './mixins/log'
+export default {
+  name: "App",
+  mixins: [log],
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/mixins/log.js
@@ -0,0 +1,5 @@
+export default {
+  created() {
+    console.log("我出生了");
+  }
+};
